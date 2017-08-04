f6010ec5b4785f883d89d60efc352f22328a801f
Ismael Thiago Marques <ismael@oc4825278545.ibm.com>
2017-08-04 17:57:49 -0300

    each

--- test_blocks.rb
+++ test_blocks.rb
@@ -48,3 +48,35 @@ end
 #it would work idented like do..end is
 #blocks that covers more than one line should be written with do..end (as a convention)
 say_braces { |word1| puts "#{word1}" }
+
+puts "========================="
+
+#using 'each' to loop through an array and execute a 'block'
+def total(prices)
+  amount = 0
+  prices.each do |price| #passing the current element in the iteration
+    amount += price
+  end	
+  amount #return
+end
+
+def refound(prices)
+  amount = 0
+  prices.each do |price| 
+    amount -= price
+  end
+  amount #return
+end
+
+def show_discounts(prices)
+  prices.each do |price|
+    amount_off = price / 3.0
+    puts format("Your discount: %.2f", amount_off)
+  end
+end
+
+prices = [3.99, 25.00, 8.99]
+puts format("%.2f", total(prices))
+puts format("%.2f", refound(prices))
+show_discounts(prices)
+
